4d9fe3fb0aea12cb86e0d54be6588670ff7e1e11
tom-8080 <peng1727507872@163.com>
2020-09-16 15:47:22 +0800

    springcloud_common first commit

--- src/main/java/com/chao/springcloud_common/pojo/Student.java
+++ src/main/java/com/chao/springcloud_common/pojo/Student.java
@@ -1,3 +1,12 @@
+package com.chao.springcloud_common.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Student{
   private Integer id;
 }
